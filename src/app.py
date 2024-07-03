7270ce663657048081d43949f8a99f521367fb39
Christian Retreta <christian.retreta@pointwest.com.ph>
2024-07-03 15:35:06 +0800

    populated utils.py for code organization

--- src/app.py
+++ src/app.py
@@ -1,117 +1,94 @@
 from dotenv import load_dotenv
-from langchain_community.utilities import SQLDatabase
 from langchain_core.messages import AIMessage, HumanMessage
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import ChatOpenAI
 import streamlit as st
 
-def init_database(user: str, password: str, host: str,
-                  port: str, database: str) -> SQLDatabase:
-    """
-    Initialize a SQLDatabase object using the given connection details.
-
-    Args:
-        user (str): The username to use for the database connection.
-        password (str): The password to use for the database connection.
-        host (str): The hostname or IP address of the database server.
-        port (str): The port number to use for the database connection.
-        database (str): The name of the database to connect to.
-
-    Returns:
-        SQLDatabase: A SQLDatabase object representing the database connection.
-    """
-    # Construct URI for database connection using mysql-connector-python package's syntax
-    db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
-
-    # Create SQLDatabase object from URI.
-    return SQLDatabase.from_uri(db_uri)
-
-def get_sql_chain(db):
-    template = """
-    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's SQL database.
-    Based on the table schema below, write a SQL query that will answer the user's question. Take the conversation history into account.
-    <SCHEMA>
-    {schema}
-    </SCHEMA>
-    Conversation history:{chat_history}
-
-    Write only the SQL query and nothing else. Do not write any other text or apply formatting.
-
-    For example: 
-    Question: Name 10 artists
-    SQL Query: SELECT name FROM artists LIMIT 10;
-
-    Now it's your turn to answer:
-
-    Question: {question}
-    SQL Query:
-    """
-    prompt = ChatPromptTemplate.from_template(template)
-    llm = ChatOpenAI(model='gpt-3.5-turbo-0125')
+from utils import init_database, get_sql_chain, get_response
 
 
+# Load environment variables from .env file
+load_dotenv()
 
-def get_schema():
-    return db.get_table_info()
-
-
-
-if "chat_history" not in st.session_state:
-    st.session_state["chat_history"] = [
-        AIMessage(content="Hello, I am a MySQL assistant. Ask me anything about your database."),
-
-
-
-    ]
-
+# Set page title and icon
 st.set_page_config(page_title='Chat with MySQL', page_icon=':speech_balloon:')
+
+# Set page title
 st.title("Chat with MySQL")
 
-with st.sidebar:
-    st.subheader("Settings")
-    st.write("This is a simple chat application using to MySQL. Connect to the database and start chatting.")
+# Flag for chat status
+if "is_disabled" not in st.session_state:
+    st.session_state["is_disabled"] = True
 
-    # key parameter saves the value to a session state
+# Sidebar settings
+with st.sidebar:
+    st.subheader("Settings")  # Sidebar header
+    # Sidebar text
+    st.write("This is a simple chat application using MySQL. Connect to the database and start chatting.")
+
+    # Input fields for database connection details
     st.text_input("Host", value="localhost", key="host")
     st.text_input("Port", value="3306", key="port")
     st.text_input("User", value="root", key="user")
     st.text_input("Password", type="password", value="admin", key="password")
-    st.text_input("Database", value="MyDatabase", key="db_name")
+    st.text_input("Database", value="world", key="db_name")
 
+    # Connect button
     if st.button("Connect"):
-        with st.spinner("Connecting to database..."):
-            db = init_database(
-                user = st.session_state["user"],
-                password = st.session_state["password"],
-                host = st.session_state["host"],
-                port = st.session_state["port"],
-                database = st.session_state["db_name"],
-            )
-            st.session_state["db_connection"] = db
-            st.success("Connected to database!")
+        try:
+            with st.spinner("Connecting to database..."):
+                # Initialize database connection
+                db = init_database(
+                    user=st.session_state["user"],
+                    password=st.session_state["password"],
+                    host=st.session_state["host"],
+                    port=st.session_state["port"],
+                    database=st.session_state["db_name"],
+                )
 
+                # Store database connection in session state
+                st.session_state["db_connection"] = db
+                st.success("Connected to database!")  # Success message
+                st.session_state["is_disabled"] = False
+        except:
+            st.error(
+                "Failed to connect to database. Please check your connection details and try again.")
 
-# Display chat history and user input
-for message in st.session_state["chat_history"]:
-    if isinstance(message, AIMessage):
-        with st.chat_message("ai"):
-            st.markdown(message.content)
-    elif isinstance(message, HumanMessage):
-        with st.chat_message("human"):
-            st.markdown(message.content)
+# Check if chat history exists in session state. If not, create a default greeting message.
+if not st.session_state["is_disabled"]:
+    if "chat_history" not in st.session_state:
+        st.session_state["chat_history"] = [
+            AIMessage(
+                content="Hello, I am a MySQL assistant. Ask me anything about your database.")
+        ]
 
-user_query = st.chat_input("Type a message...")
+    # Display chat history and user input
+    for message in st.session_state["chat_history"]:
+        if isinstance(message, AIMessage):
+            # Display AI message
+            with st.chat_message("ai"):
+                st.markdown(message.content)
+        elif isinstance(message, HumanMessage):
+            # Display human message
+                with st.chat_message("human"):
+                    st.markdown(message.content)
+
+# Get user input
+user_query = st.chat_input(
+    "Type a message...", disabled=st.session_state["is_disabled"])
+
+# If user input is not empty, process and store the input and the generated response
 if user_query and user_query.strip() != "":
-    
-    st.session_state["chat_history"].append(HumanMessage(content=user_query))
+    st.session_state["chat_history"].append(
+        HumanMessage(content=user_query))  # Store human message
 
     with st.chat_message("human"):
-        st.markdown(user_query)
+        st.markdown(user_query)  # Display human message
 
     with st.chat_message("ai"):
-        response = "I don't know how to respond to that yet, sorry!"
-        st.markdown(response)
+        sql_chain = get_sql_chain(
+            st.session_state["db_connection"])  # Generate SQL query
+        response = get_response(
+            user_query, st.session_state["db_connection"], st.session_state["chat_history"])  # Generate response
+        st.markdown(response)  # Display response
 
-    st.session_state["chat_history"].append(AIMessage(content=response))
-    
+    st.session_state["chat_history"].append(
+        AIMessage(content=response))  # Store AI message
---
+++ src/utils.py
@@ -0,0 +1,132 @@
+
+from langchain_community.utilities import SQLDatabase
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from sqlalchemy.exc import ProgrammingError
+
+
+def init_database(user: str, password: str, host: str,
+                  port: str, database: str) -> SQLDatabase:
+    """
+    Initialize a SQLDatabase object using the given connection details.
+
+    Args:
+        user (str): The username to use for the database connection.
+        password (str): The password to use for the database connection.
+        host (str): The hostname or IP address of the database server.
+        port (str): The port number to use for the database connection.
+        database (str): The name of the database to connect to.
+
+    Returns:
+        SQLDatabase: A SQLDatabase object representing the database connection.
+    """
+    # Construct URI for database connection using mysql-connector-python package's syntax
+    db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
+
+    # Create SQLDatabase object from URI.
+    return SQLDatabase.from_uri(db_uri)
+
+
+def get_sql_chain(db):
+    """
+    Returns a chain of runnables that generates a SQL query based on a user's question.
+
+    The chain first generates the schema of the database, then prompts the user with a template
+    that includes the schema and conversation history, and finally generates a SQL query using the
+    OpenAI API. The final output of the chain is a string representing the SQL query.
+
+    Parameters:
+        db (SQLDatabase): A SQLDatabase object representing the database connection.
+
+    Returns:
+        Runnable: A chain of runnables that generates a SQL query.
+    """
+    template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's SQL database.
+    Based on the table schema below, write a SQL query that will answer the user's question. Take the conversation history into account.
+    <SCHEMA>
+    {schema}
+    </SCHEMA>
+    Conversation history:{chat_history}
+
+    Write only the SQL query and nothing else. Do not write any other text or apply formatting.
+
+    For example: 
+    Question: Name 10 artists
+    SQL Query: SELECT name FROM artists LIMIT 10;
+
+    Now, it's your turn to answer:
+
+    Question: {question}
+    SQL Query:
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+    llm = ChatOpenAI(model='gpt-3.5-turbo-0125')
+
+    def get_schema(_):
+        # Returns a string info about the tables in the db
+        return db.get_table_info()
+
+    return (
+        # Assign the `schema` variable.
+        RunnablePassthrough.assign(schema=get_schema)
+        | prompt  # Prompt the user with the template.
+        | llm     # Use the OpenAI API to generate a response.
+        | StrOutputParser()  # Parse the response as a string.
+    )
+
+
+def get_response(user_query: str, db: SQLDatabase, chat_history: list) -> str:
+    """
+    Generates a natural language response to a user's SQL database question using a SQL chain and a language model.
+
+    Args:
+        user_query (str): The user's SQL database question.
+        db (SQLDatabase): The SQLDatabase object representing the database connection.
+        chat_history (list): A list of previous chat messages.
+
+    Returns:
+        str: The natural language response to the user's SQL database question or the SQL query if a ProgrammingError occurs.
+    """
+    sql_chain = get_sql_chain(db)
+
+    template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's SQL database.
+    Based on the table schema, chat history, sql query, question, and reponse below, write a nature language response that will answer the user's question.
+
+    <SCHEMA>
+    {schema}
+    </SCHEMA>
+
+    Conversation history:{chat_history}
+    SQL Query: <SQL>{query}</SQL>
+    Question: {question}
+    SQL Response: {response}    
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+    llm = ChatOpenAI(model='gpt-3.5-turbo-0125')
+
+    chain = (
+        RunnablePassthrough.assign(query=sql_chain)
+        .assign(response=lambda vars: db.run(vars["query"]))
+        .assign(schema=lambda _: db.get_table_info())
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    try:
+        res = chain.invoke({
+            "question": user_query,
+            "chat_history": chat_history
+        })
+        return res
+    except ProgrammingError as e:
+        # Extract the query from the exception message
+        error_message = str(e)
+        start = error_message.find("SQL: ") + 5
+        end = error_message.find("\n", start)
+        query = error_message[start:end-1].strip()
+        return query
